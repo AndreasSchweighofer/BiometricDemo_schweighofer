8da379e31d2395c0d71caf0fea830bb36e0fdd3f
Your Name <you@example.com>
2023-11-27 15:09:34 +0100

    Initial Commit

---
+++ BiometricDemo/BiometricDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiometricDemoApp.swift
+//  BiometricDemo
+//
+//  Created by Andreas Schweighofer on 27.11.23.
+//
+
+import SwiftUI
+
+@main
+struct BiometricDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
